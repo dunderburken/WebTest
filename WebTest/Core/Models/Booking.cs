dd89e72d236b1b013cd9c549b9b0265eff785476
Linus Wenner <dunderburken@gmail.com>
2012-12-04 10:52:37 +0100

    Minor changes

---
+++ WebTest/Core/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTest.Core.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int PersonId { get; set; }
+        public int ResourceId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+    }
+}
